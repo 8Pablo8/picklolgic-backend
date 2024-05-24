9a0dcae2904c2e3070e0b8b39b3e819392701132
8Pablo8 <pablollanamunoz@gmail.com>
2024-05-24 10:54:05 +0200

    en fin...

--- render-build.sh
+++ render-build.sh
@@ -10,8 +10,9 @@ set -o errexit
 # Verificar si el directorio de la caché de Puppeteer existe antes de copiar los archivos
 if [[ -d $PUPPETEER_CACHE_DIR ]]; then 
   echo "...Copiando la caché de Puppeteer desde la caché de compilación" 
-  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR || echo "No existe la caché de Puppeteer, se creará una nueva."
 else 
-  echo "...Almacenando la caché de Puppeteer en la caché de compilación" 
-  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+  echo "...Creando el directorio de caché de Puppeteer y almacenando la caché en la caché de compilación"
+  mkdir -p $PUPPETEER_CACHE_DIR
+  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME || echo "No se pudo copiar la caché de Puppeteer."
 fi
